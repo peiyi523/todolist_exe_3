ee851fc16ace165f96db9468427bfd79d2b13ba6
peiyi <peiyi523@gmail.com>
2024-05-01 22:19:51 +0800

    完成使用者功能頁面

--- user/urls.py
+++ user/urls.py
@@ -15,13 +15,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from app import views
+from . import views
 
-
+# 分配user次要路徑
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    # path("", views.index),
-    # 分配user主要進入點
-    path("", include("user.urls")),
+    path("register/", views.user_register),
 ]
---
+++ user/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def user_register(request):
+    return render(request, "user/register.html")
